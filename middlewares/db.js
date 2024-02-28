030cd94efecd7ed60e1aa122d5d1603cc183ef8b
jung-seung-hwan <94040225+jung-seung-hwan@users.noreply.github.com>
2024-02-28 17:29:45 +0900

    Revert "Update db.js"
    This reverts commit 6a71da495215224d8ae9b1ca19b857a6afdb240d.

--- middlewares/db.js
+++ middlewares/db.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql2");
 var db = mysql.createConnection({
-  host: "rds-shield.czmsy2g2m4cg.ap-northeast-2.rds.amazonaws.com",
+  host: "",
   user: "root",
   password: "gozld13!#",
   database: "board",
